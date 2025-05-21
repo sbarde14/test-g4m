c9fc6fbaf6d12769e185398af86a330c363d9c77
sbarde14 <marcosba.developer@gmail.com>
2025-05-21 15:31:40 +0200

    Create gam-iframe-2.js

---
+++ gam-iframe-2.js
@@ -0,0 +1,23 @@
+//   with post message
+window.addEventListener('load', function () {
+
+  // Define your image URL in a variable
+  var imageUrl = confCreative.imgSrc;
+
+  // Create the img element
+  var img = document.createElement('img');
+
+  // Set its src attribute from the variable
+  img.src = imageUrl;
+
+  // Optionally set styling or attributes
+  img.alt = '';
+  img.style.width = '100%';
+  img.style.height = 'auto';
+
+  // Append it to the body or a specific element
+  document.body.appendChild(img);
+
+
+    window.parent.postMessage({ type: 'resizeRequest' }, '*');
+});
