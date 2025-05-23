c46a8ed74d22f3c60bbb34c4d98b9b52b6d306e6
sbarde14 <marcosba.developer@gmail.com>
2025-05-23 14:29:41 +0200

    Update gam-iframe-2.js

--- gam-iframe-2.js
+++ gam-iframe-2.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function () {
 
     // Create the <a> element
     const link = document.createElement('a');
-    link.href = confCreative.clickUrlUnesc+'&adurl='+'https://www.prodottiselex.it';
+    link.href = confCreative.clickUrlUnesc+'&adurl='+confCreative.adUrl;
     link.style.display = 'block';
     link.target = '_blank';
 
